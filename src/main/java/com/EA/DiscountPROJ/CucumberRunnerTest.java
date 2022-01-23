124c845ad77fcacbfc6a6a223c32bcf25bc50ad3
eanya <voix7200@gmail.com>
2022-01-23 09:25:09 +0000

    Initial commit

---
+++ src/main/java/com/EA/DiscountPROJ/CucumberRunnerTest.java
@@ -0,0 +1,50 @@
+package com.EA.DiscountPROJ;
+
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.qa.TestBase1.TestBase1;
+import com.qa.extentReport1.ExtentReportManager1;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+ 
+
+@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
+//@CucumberOptions(features="src/test/resources/")
+@CucumberOptions(features="src/test/resources/features/offerX.feature")
+//@CucumberOptions(features={"src/test/resources/com.bbd.demo/A_login.feature",
+		//"src/test/resources/com.bbd.demo/B_contactus.feature"})
+public class CucumberRunnerTest {
+	static ExtentReports report;
+	static WebDriver driver;
+	
+	@BeforeClass
+	public static void setUp() {
+		System.out.println("before class ");		 
+		report = ExtentReportManager1.getReport();
+		
+	}
+	
+	@AfterClass
+	public static void tearDown() throws InterruptedException {
+		System.out.println("After class ");
+		 driver = TestBase1.getDriver();
+		Thread.sleep(2000); 
+		driver.get("C:\\Users\\E Anya\\eclipse-workspace1\\BACKEND_PROJ1\\ExtentFolder\\index.html"); 
+		  driver.navigate().back(); 
+		  Thread.sleep(1000); 
+		  driver.navigate().forward();
+		  report.flush(); 
+		  Thread.sleep(60000);
+		  
+		driver.quit();
+	}
+
+}                         
